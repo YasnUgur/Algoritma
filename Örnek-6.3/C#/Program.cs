6d87ad29578b3e4809f0cded0512e2fbbd5e6e6c
YasnUgur <154428117+YasnUgur@users.noreply.github.com>
2023-12-21 15:33:32 +0300

    Create Program.cs

---
+++ Örnek-6.3/C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucgenalan
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double a, b, açı, alan;
+            Console.Write("Birinci kenar uzunluğunu giriniz (cm): ");
+            a=Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci kenar uzunluğunu giriniz (cm): ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Arasındaki açıyı giriniz (derece): ");
+            açı = Convert.ToDouble(Console.ReadLine());
+
+            alan =a*b*Math.Sin(Math.PI*açı/180)/2;
+
+            Console.WriteLine("Üçgenin alanı (cm2):" + alan);
+            Console.ReadLine();
+        }
+    }
+}
