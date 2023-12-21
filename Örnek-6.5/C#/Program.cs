86d652604be00eed143fc439848511c0a015baff
YasnUgur <154428117+YasnUgur@users.noreply.github.com>
2023-12-21 23:30:50 +0300

    Create Program.cs

---
+++ Örnek-6.5/C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kenaruzun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, açı;
+            Console.Write("Birinci kenar uzunluğunu giriniz (cm): ");
+            a=Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci kenar uzunluğunu giriniz (cm): ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Arasındaki açıyı giriniz (derece): ");
+            açı = Convert.ToDouble(Console.ReadLine());
+
+            c = Math.Sqrt(a * a + b * b - 2 * a * b * Math.Cos(Math.PI*açı/180));
+
+            Console.WriteLine("C kenarın uzunluğu: "+c);
+            Console.ReadLine();
+
+        }
+    }
+}
