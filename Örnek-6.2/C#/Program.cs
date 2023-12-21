89085b3111510808aa4adebdd3a157b9144b4b3b
YasnUgur <154428117+YasnUgur@users.noreply.github.com>
2023-12-21 13:43:29 +0300

    Create Program.cs

---
+++ Örnek-6.2/C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using Algoritma;
+using System;
+using System.Runtime.Serialization;
+
+double der, rad, grad;
+double pi = 3.14;
+
+
+    Console.Write("Derece cinsinden değer giriniz: ");
+    der = Convert.ToDouble(Console.ReadLine());
+
+    rad = der * pi / 180;
+    grad = der * 200 / 180;
+
+    Console.WriteLine("Radyan: {0} \nGradyan: {1} ",rad,grad);
