fa381eec1c304051be85b741cb0591fd7c26bef8
YasnUgur <154428117+YasnUgur@users.noreply.github.com>
2023-12-22 13:22:58 +0300

    Create Program.cs

---
+++ Örnek-6.6/C#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dortgenalan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double e, f, açı, alan;
+            Console.Write("Birinci köşegenin uzunluğunu giriniz (cm): ");
+            e=Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci köşegenin uzunluğunu giriniz (cm): ");
+            f = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Arasındaki açıyı giriniz (derece): ");
+            açı = Convert.ToDouble(Console.ReadLine());
+
+            alan=e*f*Math.Sin(Math.PI*açı/180)/2;
+
+            Console.WriteLine("Dörtgenin alanı: {0:F3}",alan);
+            Console.ReadLine();
+        }
+    }
+}
