12acd15cbf99004f7827ad185e08df1b6514520b
YasnUgur <154428117+YasnUgur@users.noreply.github.com>
2023-12-25 10:13:15 +0300

    Create Program.cs

---
+++ Örnek-6.18/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekçift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı;
+            Console.Write("Bir tam sayı giriniz: ");
+            sayı=Convert.ToInt32(Console.ReadLine());
+
+            if (sayı % 2 == 0)
+                Console.WriteLine("Girilen sayı çift bir sayıdır.");
+            else
+                Console.WriteLine("Girilen sayı tek bir sayıdır.");
+
+            Console.ReadLine();
+        }
+    }
+}
