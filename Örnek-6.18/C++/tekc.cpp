51a9ff9224c93efe6367e8dbe54b6c184e77c17e
YasnUgur <154428117+YasnUgur@users.noreply.github.com>
2023-12-25 10:13:17 +0300

    Create tekc.cpp

---
+++ Örnek-6.18/C++/tekc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+	int sayi;
+	cout<<"Bir tamsyi giriniz: ";
+	cin>>sayi;
+	
+	if(sayi%2 == 0)
+	cout<<"Girilen sayi cift bir sayidir.";
+	else 
+	cout<<"Girilen sayý tek bir sayidir.";
+	
+	
+	return 0;
+}
